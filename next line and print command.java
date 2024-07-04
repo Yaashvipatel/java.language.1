23e3ad1872b16bf68a40b835eca351160c96bd40
yaashvi patel <151950628+Yaashvipatel@users.noreply.github.com>
2024-07-04 13:30:15 +0530

    Create next line and print command.java

---
+++ next line and print command.java
@@ -0,0 +1,17 @@
+//next line and print command in java 
+public class Main
+{
+	public static void main(String[] args) {
+	    //output in newline : 
+	    System.out.println("hi");
+		System.out.print("hello");
+		System.out.println("this");
+		System.out.println("is me");
+		
+		//newline using "\n" :
+		System.out.print("hi\n");
+		System.out.print("this is me ");
+		
+		
+	}
+}
