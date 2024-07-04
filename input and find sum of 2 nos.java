29d584676a4f987e1712c6a136fd0d1754c0ac80
yaashvi patel <151950628+Yaashvipatel@users.noreply.github.com>
2024-07-04 13:31:32 +0530

    Create input and find sum of 2 nos.java

---
+++ input and find sum of 2 nos.java
@@ -0,0 +1,15 @@
+//input and find sum of 2 nos.java
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		int sum;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter no 1 : ");
+		int a = sc.nextInt();
+		System.out.println("enter no 2 : ");
+		int b = sc.nextInt();
+		sum=a+b;
+		System.out.print(sum);
+	}
+}
